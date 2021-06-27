5eb05df46775af9467a64d8a4e7d9f238ce0e374
Alon Sirota <sirotaalon@gmail.com>
2021-06-28 00:50:33 +0300

    Add assembler.c/h & run assemble on argv filenames

---
+++ assembler.c
@@ -0,0 +1,23 @@
+//
+// Created by alon on 6/27/21.
+//
+#include "assembler.h"
+
+int main(int argc, char *argv[]) {
+    int i;
+    if (argc < 1) {
+        printf("Error: You need at least one argument");
+        return 1;
+    }
+
+    /* Assemble every filename in arguments. */
+    for (i = 0; i < argc; i++){
+        assemble(argv[i]);
+    }
+
+    return 0;
+}
+
+void assemble(char *fileName) {
+    /* TODO */
+}
---
+++ assembler.h
@@ -0,0 +1,10 @@
+//
+// Created by alon on 6/27/21.
+//
+
+#ifndef ASSEMBLER_ASSEMBLER_H
+#define ASSEMBLER_ASSEMBLER_H
+#include <stdio.h>
+
+void assemble(char *fileName);
+#endif //ASSEMBLER_ASSEMBLER_H
