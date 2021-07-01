1cc8690089baff608f9b63d96bea9d3b3623ddc9
Alon Sirota <sirotaalon@gmail.com>
2021-07-02 00:56:49 +0300

    Add line processing skeleton

---
+++ src/line.c
@@ -0,0 +1,6 @@
+//
+// Created by alon on 7/1/21.
+//
+
+#include "line.h"
+
---
+++ src/line.h
@@ -0,0 +1,18 @@
+//
+// Created by alon on 7/1/21.
+//
+
+#ifndef WTFPROJECT_LINE_H
+#define WTFPROJECT_LINE_H
+typedef struct node {
+    char *value;
+    struct node *next;
+} node;
+
+typedef struct line {
+    char *label;
+    node head;
+} line;
+
+line strToLine(char *str);
+#endif //WTFPROJECT_LINE_H
