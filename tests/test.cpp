d43e6569fdd593e4bb127e520f3aa1fd3d5291de
Alon Sirota <sirotaalon@gmail.com>
2021-07-03 16:36:37 +0300

    added fmemopen support for windows and strsep implementation

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,9 @@
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#include "./fmemopen_windows.c"
+#endif
 #include "gtest/gtest.h"
 #include "assembler.c"
 #include "line.c"
-#include "./fmemopen_windows.c"
 
 TEST(fgetsShred, Shred) {
     FILE *stream;
