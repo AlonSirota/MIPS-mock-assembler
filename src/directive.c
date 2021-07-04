b696336929d383143632bf5f4325582f0e2957e9
Alon Sirota <sirotaalon@gmail.com>
2021-07-05 01:40:04 +0300

    guidance renamed to directiveType + refactors

---
+++ src/directive.c
@@ -0,0 +1,22 @@
+//
+// Created by Ram Ben on 7/3/2021.
+//
+
+#include "directive.h"
+
+directiveType strToDirectiveType(char *name){
+    if(name == NULL)
+        return UNDEFINED;
+
+    if (strcmp(".db", name)) {
+        return BYTE;
+    } else if (strcmp(".dh", name)) {
+        return HALF_WORD;
+    } else if (strcmp(".dw", name)) {
+        return WORD;
+    }else if (strcmp(".asciz", name)) {
+        return ASCII;
+    }else {
+        return UNDEFINED;
+    }
+}
---
+++ src/directive.h
@@ -0,0 +1,21 @@
+//
+// Created by alon on 7/4/21.
+//
+
+#ifndef ASSEMBLER_DIRECTIVE_H
+#define ASSEMBLER_DIRECTIVE_H
+#include <stdio.h>
+#include <string.h>
+#include "line.h";
+
+typedef enum{
+    WORD,
+    BYTE,
+    HALF_WORD,
+    ASCII,
+    UNDEFINED
+} directiveType;
+int isLineDirective(line l);
+directiveType strToDirectiveType(char *name);
+
+#endif //ASSEMBLER_DIRECTIVE_H
