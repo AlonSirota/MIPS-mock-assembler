0864b05d20a9bb5dbfe25fec174eb6dd6cea258d
Alon Sirota <sirotaalon@gmail.com>
2021-08-21 13:03:39 +0300

    adding trivial codee

--- tests/test.cpp
+++ tests/test.cpp
@@ -860,10 +860,10 @@ TEST(printLineToBuffer, print){
 }
 
 TEST(assemble, ps) {
-//    std::string buf = asFilesBasePath + "ps.as";
-//    std::string buf2 = asFilesBasePath + "true_ps.ob";
-//    char yolo[100];
-//    strcpy(yolo, buf.c_str());
+    std::string buf = asFilesBasePath + "ps.as";
+    std::string buf2 = asFilesBasePath + "true_ps.ob";
+    char yolo[100];
+    strcpy(yolo, buf.c_str());
 //    assemblePath(yolo);
 //    std::string buf3 = asFilesBasePath + "ps.ob";
 //    ASSERT_TRUE(compareFiles(buf2,buf3));
