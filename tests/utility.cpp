e68b42d45e6fa695f4fcc60d4c004beff44bb041
Ram Ben <ramben13@gmail.com>
2021-08-22 17:59:15 +0300

    fixed file comparing. now ignores '\r' and trailing new lines in the end of files

--- tests/utility.cpp
+++ tests/utility.cpp
@@ -13,15 +13,32 @@ bool compareFiles(const std::string& p1, const std::string& p2) {
         fprintf(stderr, "Failed to open a file\n");
         return false; //file problem
     }
-
+    /*
     if (f1.tellg() != f2.tellg()) {
         printf("Files are of different lengths\n");
         return false; //size mismatch
     }
-
+*/
     //seek back to beginning and use std::equal to compare contents
     f1.seekg(0, std::ifstream::beg);
     f2.seekg(0, std::ifstream::beg);
+
+    char c1;
+    char c2;
+
+    while (!f1.eof() && !f2.eof()){
+        while (!f1.eof() && (c1 = f1.get()) != '\r');
+        while (!f2.eof() && (c2 = f2.get()) != '\r');
+        if(c1 != c2){
+            while (!f1.eof() && ((c1 = f1.get()) != '\r' || c1 != '\n'));
+            while (!f2.eof() && ((c2 = f2.get()) != '\r' || c2 != '\n'));
+            if(f1.eof() && f2.eof())
+                return true;
+            return false;
+        }
+    }
+    return true;
+
     return std::equal(std::istreambuf_iterator<char>(f1.rdbuf()),
                       std::istreambuf_iterator<char>(),
                       std::istreambuf_iterator<char>(f2.rdbuf()));
