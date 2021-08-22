74f68e81f6793d42c6423359e8ce850c615f0391
Alon Sirota <sirotaalon@gmail.com>
2021-08-23 00:11:32 +0300

    declare stdup in helper.h

--- src/helper.h
+++ src/helper.h
@@ -13,6 +13,7 @@ int parseLong(const char *str, long *val);
 char *fgetsShred(FILE *f, int n, char *buffer);
 char* strsep(char** stringp, const char* delim);
 char *strdupN(const char *original, int n);
+char *strdup(const char *src);
 char lastChar(char *str);
 
 #endif
